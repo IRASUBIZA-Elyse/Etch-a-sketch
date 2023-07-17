e335a27afe87716edab38e4871fdcc061bfee687
IRASUBIZA Elyse <12356789+odin@users.norely.github.com>
2023-07-17 12:59:41 +0200

    added click functions on the buttons

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
+function populateBoard(size){
     let board = document.querySelector(".board");
-    board.style.gridTemplateColumns =`repeat( 16 , 1fr`;
-    board.style.gridTemplateRows = `repeat( 16 , 1fr)`;
+    let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns =`repeat(${size} , 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-
-for (let i = 0; i <256 ; i++){
+let amount = size*size
+for (let i = 0; i <amount ; i++){
     let square = document.createElement("div");
-    square.style.backgroundColor = "yellow";
+    square.addEventListener("mouseenter",colorSquare);
+    square.style.backgroundColor = "#f8f8ff";
     board.appendChild(square);
 }
+}
+populateBoard(16);
 
-
-
+function changeSize(input) {
+    if (input >= 2 && input <= 150) {
+        populateBoard(input);
+      } else {
+        alert("too many don't exceed 150")
+      }
+  }
+  let color = 'black'
+  function colorSquare() {
+    if (color === "random") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+      } else {
+        this.style.backgroundColor = color;
+      }
+  }
+  function changeColor(choice){
+    color = choice;
+  }
+  function resetBoard() {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => (div.style.backgroundColor = "white"));
+  }
